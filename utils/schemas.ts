307911db68e24ce50509f26ec42e35ce62e899ed
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-02-28 15:42:55 +0000

    update validation rules and error messages

--- utils/schemas.ts
+++ utils/schemas.ts
@@ -1,7 +1,48 @@
 import * as z from "zod";
+import { ZodSchema } from "zod";
 
 export const profileSchema = z.object({
-  firstName: z.string().min(2 , {message: "firstName must at leat contain 2 character"}),
-  lastName: z.string().min(3),
-  username: z.string().min(3),
+  firstName: z
+    .string()
+    .min(2, { message: "first name must at least 2 characters" }),
+  lastName: z
+    .string()
+    .min(2, { message: "last name must at least 2 characters" }),
+  username: z
+    .string()
+    .min(2, { message: "username must at least 2 characters" }),
 });
+
+
+function validateFile() {
+  const maxUploadSize = 1024 * 1024;
+  const acceptedFileTypes = ["image/"];
+
+  return z
+    .instanceof(File)
+    .refine(
+      (file) => file.size <= maxUploadSize,
+      "File size must be less than 1 MB",
+    )
+    .refine(
+      (file) => acceptedFileTypes.some((type) => file.type?.startsWith(type)),
+      "File must be an image",
+    );
+}
+
+export const imageSchema = z.object({
+  image: validateFile(),
+});
+
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown,
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.errors.map((error) => error.message);
+
+    throw new Error(errors.join(", "));
+  }
+  return result.data;
+}
