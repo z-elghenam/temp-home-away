fb4d67cbc493f0607e58e6b40e745a4762714b44
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-03-02 09:31:45 +0000

    handle create new property

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "img.clerk.com" },
+      {
+        protocol: "https",
+        hostname: "rurzgdtbaxnolcybaipn.supabase.co",
+        pathname: "/storage/v1/object/public/temp-home-away/**",
+      },
+    ],
+  },
+    experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb", // Increase the limit to 10 MB (or any size you need)
+    },
+  },
 };
 
 export default nextConfig;
--- utils/db.ts
+++ utils/db.ts
@@ -1,17 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
